0d1d9f8c0ed254a17c770aa1238da73b49e1e001
Dev_WaiYan <htetwaiwilsonsprite@gmail.com>
2024-06-30 20:25:12 +0630

    [ENHANCEMENT] project setup with ant design and add router

---
+++ src/components/common/Navigation.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Menu, Drawer, Button, MenuProps } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+import { useMediaQuery } from "react-responsive";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { HomeOutlined, SettingOutlined } from "@ant-design/icons";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+  {
+    label: "Home",
+    key: "/",
+    icon: <HomeOutlined />,
+  },
+  {
+    label: "Setting",
+    key: "/setting",
+    icon: <SettingOutlined />,
+  },
+];
+
+const Navigation: React.FC = () => {
+  const [visible, setVisible] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [current, setCurrent] = useState(location.pathname);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    setCurrent(e.key);
+    navigate(e.key)
+  };
+
+  const menuItems = (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode={isMobile ? "vertical" : "horizontal"}
+      items={items}
+    />
+  );
+
+  return (
+    <>
+      {isMobile ? (
+        <>
+          <Button type="primary" onClick={showDrawer}>
+            <MenuOutlined />
+          </Button>
+          <Drawer
+            title="Menu"
+            placement="right"
+            onClose={onClose}
+            open={visible}
+          >
+            {menuItems}
+          </Drawer>
+        </>
+      ) : (
+        menuItems
+      )}
+    </>
+  );
+};
+
+export default Navigation;
